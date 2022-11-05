fb1142eee86e895d0c792bff8cfc95e7222de418
Mr.Fumpledump <Fumpledump@gmail.com>
2022-11-05 09:25:07 -0400

    Added Camera Follow Script

--- GameMusicJam2022/Assets/Script/CameraFollow.cs
+++ GameMusicJam2022/Assets/Script/CameraFollow.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform target;
 
-    // Update is called once per frame
-    void Update()
+    public float smoothSpeed = 10f;
+    public Vector3 offset;
+
+    private void LateUpdate()
     {
-        
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
     }
 }

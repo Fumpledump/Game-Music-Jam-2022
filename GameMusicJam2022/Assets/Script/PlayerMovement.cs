d8e4bb1390905e0e8db18bbd00294e540cf117fc
Mr.Fumpledump <Fumpledump@gmail.com>
2022-11-06 20:07:57 -0500

    Gliding Done

--- GameMusicJam2022/Assets/Script/PlayerMovement.cs
+++ GameMusicJam2022/Assets/Script/PlayerMovement.cs
@@ -14,10 +14,45 @@ public class PlayerMovement : MonoBehaviour
     public bool controlsOn;
     public float speed = 8f; // Movement Speed of the Player
     public float jumpForce = 10f; // How far the player can jump
+    public float glideSpeed;
     public Vector2 moveDirection = Vector2.zero;
 
     private bool jump;
     private bool facingRight = true; // Direction of the sprite
+    private float intialGravityScale;
+
+    private void Start()
+    {
+        controlsOn = true;
+        intialGravityScale = rb.gravityScale;
+    }
+
+    private void Update()
+    {
+        // Jumping
+        if (jump && isGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        // Gliding
+        if (jump && rb.velocity.y <= 0)
+        {
+            rb.gravityScale = 0;
+            rb.velocity = new Vector2(rb.velocity.x, -glideSpeed);
+        }
+        else
+        {
+            rb.gravityScale = intialGravityScale;
+        }
+
+        //Flip();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveDirection.x * speed, rb.velocity.y);
+    }
 
     public void SetMovement(InputAction.CallbackContext context)
     {
@@ -33,25 +68,6 @@ public class PlayerMovement : MonoBehaviour
         {
             jump = context.ReadValueAsButton();
         }
-    }
-
-    private void Start()
-    {
-        controlsOn = true;
-    }
-    private void Update()
-    {
-        if (jump && isGrounded())
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-        }
-
-        //Flip();
-    }
-
-    private void FixedUpdate()
-    {
-        rb.velocity = new Vector2(moveDirection.x * speed, rb.velocity.y);
     }
 
     private void Flip()

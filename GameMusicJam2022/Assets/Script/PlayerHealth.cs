e982d2b2d402111ed8739270ae580d7464e83ee0
Mr.Fumpledump <Fumpledump@gmail.com>
2022-11-09 12:43:56 -0500

    Working Health UI and Visual Fixes

--- GameMusicJam2022/Assets/Script/PlayerHealth.cs
+++ GameMusicJam2022/Assets/Script/PlayerHealth.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     public float health;
     public float maxHealth;
-    private bool dead;
+    public bool dead;
+
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +25,27 @@ public class PlayerHealth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < health)
+            {
+                hearts[i].sprite = fullHeart;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+
+            if (i < maxHealth)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+
         if(dead == true)
         {
             Debug.Log(dead);

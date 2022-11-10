f9e384949d812d482646523a0d5644932419e4e5
Mr.Fumpledump <Fumpledump@gmail.com>
2022-11-10 01:48:04 -0500

    Art & Collision Fixes

--- GameMusicJam2022/Assets/Script/Bullet.cs
+++ GameMusicJam2022/Assets/Script/Bullet.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float projectileSpeed;
+    public int direction;
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.Translate(-projectileSpeed * Time.deltaTime, 0, 0);
+        this.transform.Translate((projectileSpeed * direction) * Time.deltaTime, 0, 0);
         Destroy(gameObject,5);
     }
     

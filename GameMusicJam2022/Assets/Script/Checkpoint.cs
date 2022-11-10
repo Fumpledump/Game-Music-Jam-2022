ec506ecae1e1b92fa5829367d17d74d636705f83
Ninjasquirrel220 <tyler.samay@hotmail.com>
2022-11-09 21:06:36 -0500

    Respawn and CheckPoint

--- GameMusicJam2022/Assets/Script/Checkpoint.cs
+++ GameMusicJam2022/Assets/Script/Checkpoint.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
-
     public float posX;
     public float posY;
     public float posZ;
     // Start is called before the first frame update
     void Start()
     {
+        //Gets the positions of this checkpoint
         posX = this.transform.position.x;
         posY = this.transform.position.y;
         posZ = this.transform.position.z;
@@ -25,14 +25,13 @@ public class Checkpoint : MonoBehaviour
     //If player reaches checkpoint
     private void OnTriggerEnter2D(Collider2D col)
     {
+        //Collides with player
         if(col.gameObject.tag == "Player")
         {
+            //Saves x,y,and z position
             PlayerPrefs.SetFloat("posX", posX);
             PlayerPrefs.SetFloat("posY", posY);
             PlayerPrefs.SetFloat("posZ", posZ);
-            Debug.Log(posX);
-            Debug.Log(posY);
-            Debug.Log(posZ);
         }
         
     }
